dbe744dba9967cbfe2295fa9e92f19d41c8c5635
Masrukh Ahmed <masrukhahmed502@gmail.com>
2026-01-05 13:29:41 +0530

    Implemented logic for displaying custom weather icons, based on API weather codes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const apiKey = "aa2ead71cdb370f50fc4c5f0c6b28eec"; // Replace with your actual A
 const searchInput = document.querySelector(".search-area input");
 const cityWithCountry = document.querySelector(".city-name");
 const todayDate = document.querySelector(".date");
-const weatherIcon = document.querySelector(".weather-visual");
+const weatherIcon = document.querySelector(".weather-visual img");
 const mainTemp = document.querySelector(
   ".temp-display-description .temp-value"
 );
@@ -21,6 +21,31 @@ const cloud = document.querySelector(".cloud-details .detail-value");
 function prepareAPIurl(placeName) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${placeName}&appid=${apiKey}&units=metric`; // Added units=metric for Celsius
   return apiUrl;
+}
+
+function fetchCustomWeatherIcon(weatherCode) {
+  const iconPack = {
+    "01d": "01d.svg",
+    "01n": "01n.svg",
+    "02d": "02d.svg",
+    "02n": "02n.svg",
+    "03d": "03d.svg",
+    "03n": "03n.svg",
+    "04d": "04d.svg",
+    "04n": "04n.svg",
+    "09d": "09d.svg",
+    "09n": "09n.svg",
+    "10d": "10d.svg",
+    "10n": "10n.svg",
+    "11d": "11d.svg",
+    "11n": "11n.svg",
+    "13d": "13d.svg",
+    "13n": "13n.svg",
+    "50d": "50d.svg",
+    "50n": "50n.svg",
+  };
+  let iconFileName = `${iconPack[weatherCode]}` || "01d.svg"; //mistake here - understood the difference between object bracket notation and dot notation and where each is needed
+  return `assets/icons/${iconFileName}`;
 }
 
 async function fetchData(url) {
@@ -42,13 +67,13 @@ async function info(apiUrl) {
   const data = await fetchData(apiUrl);
   const cityName = `${data.data.name}, ${data.data.sys.country}`;
   const today = new Date();
-  const temp = data.data.main.temp;
+  const temp = Math.round(data.data.main.temp);
   const description = data.data.weather[0].description;
   const iconCode = data.data.weather[0].icon;
-  const maxTemp = data.data.main.temp_max;
-  const minTemp = data.data.main.temp_min;
+  const maxTemp = data.data.main.temp_max.toFixed(1);
+  const minTemp = data.data.main.temp_min.toFixed(1);
   const humidityDetail = data.data.main.humidity;
-  const windDetail = data.data.wind.speed * 3.6; //converting to km/h from m/s since api returns m/s units
+  const windDetail = (data.data.wind.speed * 3.6).toFixed(1); //converting to km/h from m/s since api returns m/s units
   const cloudDetail = data.data.clouds.all;
   // console.log({
   //   cityName,
@@ -89,9 +114,11 @@ async function updateUI(url) {
     windDetail,
     cloudDetail,
   } = await info(url); //destructuring the incoming data - useful
+  //ui updation of each part of the weather detail card
   cityWithCountry.innerHTML = cityName;
   todayDate.innerHTML = `${formatDate(today)}`;
   mainTemp.innerHTML = `${temp}°`;
+  weatherIcon.src = `${fetchCustomWeatherIcon(iconCode)}`;
   mainWeatherDescription.innerHTML = description;
   highTemp.innerHTML = `${maxTemp}°C`;
   lowTemp.innerHTML = `${minTemp}°C`;
